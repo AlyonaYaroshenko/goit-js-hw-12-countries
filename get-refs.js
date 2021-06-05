64bc015c7021ef858a31aa464b7837144e968865
AlyonaYaroshenko <alyona.yaroshenko03@gmail.com>
2021-06-06 02:54:23 +0300

    added alert

---
+++ get-refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    countryContainer: document.querySelector('.js-countries-container'),
+    searchForm: document.querySelector('.js-search-form'),
+  };
+}
--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 import './css/styles.css';
+import { alert } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import countryMarkUp from './templates/country-card.hbs';
-const refs = {
-    countryContainer: document.querySelector('.js-countries-container')
+import getRefs from './get-refs'; 
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const searchQuery = form.elements.query.value;
+   
+    fetchCountry(searchQuery)
+        .then(renderCountryCard)
+        .catch(onFetchError)
+        .finally(() => form.reset());
 }
 
-fetch('https://restcountries.eu/rest/v2/name/eesti')
-    .then(response => {
-        return response.json();
-    })
-    .then(country => {
-        console.log(country);
-        refs.countryContainer.innerHTML = countryMarkUp(country);
-    }).catch(error => {
-        console.log(error);
-    });
+function fetchCountry(countryId) {
+    const url = 'https://restcountries.eu/rest/v2';
+    return fetch(url)
+        .then(response => response.json());
+};
+
+function renderCountryCard(country) {
+  const markup = countryMarkUp(country);
+  refs.countryContainer.innerHTML = markup;
+};
+
+function onFetchError(error) {
+    alert('Ooops! Too many options. Specify the name of the country.');
+}
